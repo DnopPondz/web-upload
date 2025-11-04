b745399c907b44ebde60822bb7cdf06283260c19
pete <pete@movaci.com>
2025-11-04 13:39:06 +0700

    fix2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,19 @@ import { Sour_Gummy, Itim } from "next/font/google";
 const sourGummy = Sour_Gummy({
   subsets: ["latin"],
   weight: "400",
+  variable: "--font-sourgummy",
 });
 
 const itim = Itim({
   subsets: ["latin", "thai"],
   weight: "400",
+  variable: "--font-itim",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${sourGummy.className} ${itim.variable}`}>
+    <main className={`${sourGummy.variable} ${itim.variable}`}>
       <Component {...pageProps} />
     </main>
   );
 }
-
